02357598e905f4b1ae48a5336d6923358b56808d
Lukas <razilgry@gmail.com>
2023-12-09 06:50:21 +0100

    Dodaj pliki projektów.

---
+++ KontrolaDostepu/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+        Console.Write("Type your age:");
+        
+        byte age = Convert.ToByte(Console.ReadLine());
+
+
+        if (age > 18)
+        {
+            Console.WriteLine("Access granted");
+        }
+        else if (age < 15)
+        {
+            Console.WriteLine("Access denied");
+        }
+        else if (age >= 15 && age <= 18)
+        {
+            Console.WriteLine("Do you have parental consent? (yes / no)");
+            if (Console.ReadLine() == "yes") 
+            {
+                Console.WriteLine("Access granted");
+            }
+            else 
+            { 
+                Console.WriteLine("Access denied"); 
+            }
+               
+           
+        }
+        Console.ReadKey();
+    }
+}
